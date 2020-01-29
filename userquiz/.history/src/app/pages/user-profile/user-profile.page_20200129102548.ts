51c8e8e560ddae738c52847add5f32afd24a0a04
Shashanka <anilg@techinflo.com>
2020-01-29 11:12:48 +0530

    SS changed login page design

---
+++ userquiz/.history/src/app/pages/user-profile/user-profile.page_20200129102548.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/shared/user.service';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.page.html',
+  styleUrls: ['./user-profile.page.scss'],
+})
+export class UserProfilePage implements OnInit {
+
+  userDetails;
+  constructor(
+    private userService: UserService,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+
+    this.userService.getUserProfile().subscribe(
+      res => {
+        this.userDetails = res["user"];
+      },
+      err => { }
+    );
+  }
+
+  onLogout() {
+    this.userService.deleteToken();
+    this.router.navigate(["/login"]);
+  }
+  goToQuiz() {
+    this.router.navigate(["/quiz"]);
+  }
+
+}
